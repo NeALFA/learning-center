38a8cb003b71fc6fecb694acab7d59d8175fdb4e
NeAlfa <sharipovdevelopper@gmail.com>
2023-07-18 05:39:20 -0700

    first commit

---
+++ js/const.js
@@ -0,0 +1,13 @@
+const LIMIT = 8;
+const ENDPOINT = "https://64b3f1890efb99d8626890b8.mockapi.io/api/v2/";
+
+const addBtn = document.querySelector(".add");
+const editBtn = document.querySelector(".edit-btn");
+const deleteBtn = document.querySelector(".delet-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector("#modal");
+const form = document.querySelector(".form");
+const modalBtn = document.querySelector(".modal-btn");
+const filterSelect = document.querySelector(".filter-select");
+const marriedSelect = document.querySelector(".married-select");
+const searchInput = document.querySelector(".search-input");
---
+++ js/students.js
@@ -0,0 +1,135 @@
+const url = new URLSearchParams(location.search);
+const teacher = url.get("teacher");
+const studentsCard = document.querySelector(".students-card_container");
+console.log(teacher);
+
+let search = "";
+let selected = null;
+let order = "asc";
+let students = "";
+///////////// Function students
+
+async function getStudentsData() {
+  console.log("Error");
+  studentsCard.innerHTML = "Loading...";
+  const res = await request.get(`teacher/${teacher}/students`);
+  console.log(res.data);
+  studentsCard.innerHTML = "";
+  res.data.map((teacher) => {
+    studentsCard.innerHTML += getStudentsCard(teacher);
+  });
+}
+getStudentsData();
+function getStudentsCard(student) {
+  return `
+          <div class="card">
+          <div class="card__title">
+            <div class="card-img">
+              <img
+                src="${student.avatar}"
+                alt="IMG"
+              />
+            </div>
+            <div class="card-title">
+              <h1 class="firstName">${student.firstName}</h1>
+              <span><h1 class="lastName">${student.lastName}</h1></span>
+              <span><p>| ID: ${student.id}</p></span>
+            </div>
+          </div>
+          <div class="card_descripion">
+            <div class="social">
+              <div class="gmail">
+                <a href="${student.email}">
+                <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M140-160q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h680q24 0 42 18t18 42v520q0 24-18 42t-42 18H140Zm340-302L140-685v465h680v-465L480-462Zm0-60 336-218H145l335 218ZM140-685v-55 520-465Z"/></svg>
+                </a>
+              </div>
+              <div class="instagram">
+                <a href="https://www.instagram.com">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                </a>
+              </div>
+              <div class="telegram">
+                <a href="t.me:@AS5171">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>
+                </a>
+              </div>
+            </div>
+            <div class="groups">
+              <a href="tel:+998996492639">Phone:${student.phoneNumber}</a>
+              <p>isMarried: ${student.isMarried ? "Married" : "Single"}</p>
+            </div>
+            <div class="buttons">
+              <button class="btn edit-btn" onclick="openModal(${
+                student.id
+              })">Edit</button>
+              <button class="btn delet-btn" onclick="deleteStudentData(${
+                student.id
+              })">Delet</button> 
+              <button style="display:none;"></button>
+            </div>
+          </div>
+        </div>
+          `;
+}
+
+/////////////////////// Function deleted
+async function deleteStudentData(studentid) {
+  await request.delete(`teacher/${teacher}/students/${studentid}`);
+  getStudentsData();
+}
+
+////////////////// Function modal
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  let student = {
+    firstName: this.firstName.value,
+    lastName: this.lastName.value,
+    email: this.email.value,
+    phoneNumber: this.phoneNumber.value,
+    isMarried: this.chek.checked,
+  };
+  if (selected === null) {
+    await request.post(`teacher/${teacher}/students`, student);
+  } else {
+    await request.put(`teacher/${teacher}/students/${selected}`, student);
+  }
+  this.reset();
+  getStudentsData();
+});
+
+async function openModal(studentid) {
+  modal.style.top = 0;
+  let { data } = await request.get(`teacher/${teacher}/students/${studentid}`);
+  selected = studentid;
+  console.log(data);
+  modalBtn.textContent = "Edit student date";
+  document.getElementById("firstName").value = data.firstName;
+  document.getElementById("lastName").value = data.lastName;
+  document.getElementById("phoneNumber").value = data.phoneNumber;
+  document.getElementById("email").value = data.email;
+  document.getElementById("chek").checked = data.isMarried;
+}
+
+addBtn.addEventListener("click", () => {
+  modal.style.top = 0;
+  selected = null;
+  modalBtn.textContent = "Add student";
+});
+
+closeBtn.addEventListener("click", function modalCloseFunction() {
+  modal.style.top = "-150%";
+});
+
+/////////////////////// Function filter
+filterSelect.addEventListener("change", function () {
+  order = this.value;
+  getStudentsData();
+});
+
+const openStudents = (id) => {
+  window.location = `students.html?teacher=${id}`;
+};
+
+modalBtn.addEventListener("click", function closeMoDal() {
+  modal.style.top = "-150%";
+});
